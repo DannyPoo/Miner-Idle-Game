d8bed4cc94af77d37021af115fca003fc9f47f97
DannyPoo <danielulad1992@hotmail.com>
2023-06-14 03:31:50 -0400

    Updated Visuals
    visuals completed

--- miningGame.js
+++ miningGame.js
@@ -1,32 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("buttonsContainer").addEventListener("click", function(event) {
     const target = event.target;
+    const mineralType = target.getAttribute("data-mineral");
 
     if (target.tagName.toLowerCase() === "button") {
-      const mineralType = target.getAttribute("data-mineral");
-
       if (target.id.startsWith("mine")) {
-        if (mineralType === "coal") {
+        if (mineralType === "coal" || mineralType === "gold") {
           const canvas = document.createElement("canvas");
           canvas.width = target.offsetWidth;
           canvas.height = target.offsetHeight;
           const context = canvas.getContext("2d");
           const image = new Image();
-          image.src = "coal.png";
+          image.src = `${mineralType}.png`;
           image.crossOrigin = "Anonymous";
           image.onload = function() {
             context.drawImage(image, 0, 0, canvas.width, canvas.height);
-            const pixelData = context.getImageData(
-              event.offsetX,
-              event.offsetY,
-              1,
-              1
-            ).data;
+            const pixelData = context.getImageData(event.offsetX, event.offsetY, 1, 1).data;
             if (pixelData[3] !== 0) {
               mineMineral(mineralType);
             }
           };
-
         } else {
           mineMineral(mineralType);
         }
@@ -52,14 +45,10 @@ document.addEventListener("DOMContentLoaded", function() {
       document.getElementById("prestigeScreen").style.display = "none";
       document.getElementById("miningScreen").style.display = "block";
     });
-});
+  });
 
-
-  
   document.getElementById("prestigeBtn").addEventListener("click", function(){
     prestige();
   })
+
 });
-document.getElementById("statsScreen").style.display = "none";
-document.getElementById("prestigeScreen").style.display = "none";
-document.getElementById("miningScreen").style.display = "block";
